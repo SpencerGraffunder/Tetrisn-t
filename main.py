9765f227055fbcaa7a7748201840ebd3374f80d0
Spencer Graffunder <spencer@SpencersMBP935.home>
2019-12-01 08:50:17 -0600

    add some comments

--- main.py
+++ main.py
@@ -1,47 +1,66 @@
 import pygame
 import pdb
 import os
-from Tile import *
+from Tile import * # Import like this to avoid having to do Tile. before everything
 from Piece import *
 import random
 from tetrisnt_enums import *
 
+# window dimensions
 window_height = 400
 window_width = 400
+
+# control delay between frames
 frame_rate = 10
 frame_delay = 1000//frame_rate
+
+# board dimensions
 board_height = 20
 board_height_buffer = 2 # buffer for blocks that start above the top of the board
 board_width = 10
+
+# calculate size of one tile based on board height
 tile_size = window_height // board_height
-images = {}
+
+# store sprites
+sprites = {}
+
+# stores Piece object that holds data about active piece
 active_piece = None
+
+# game variables
 score = 31415
-debug_string = 'hello there'
 time_to_spawn = False
+
+# string to display on screen with debugging info
+debug_string = 'hello there'
 
 # 2d array where all the tiles are stored, initialized with board_width * (board_height + board_height_buffer) blank tiles
 # access with board[col][row]
+# initialized in init_board()
 board = []
 
+# Fill board with empty tiles
 def init_board():
-	global board
+	global board # must do this to tell it you're going to modify the global variable, else it'll make a new one
+
 	board = [[Tile() for j in range(board_width)] for i in range(board_height+board_height_buffer)]
 
-def load_images():
-	images[0] = pygame.image.load('backgroundblock.bmp')
-	images[1] = pygame.image.load('ISTblock.bmp')
+
+def load_sprites():
+	sprites[0] = pygame.image.load('backgroundblock.bmp')
+	sprites[1] = pygame.image.load('ISTblock.bmp')
 
 def draw_board():
 	screen.fill((0,0,0))
 	for row, tile_row in enumerate(board[2:]):
 		for col, tile in enumerate(tile_row):				
 
-			scaled_image = pygame.transform.scale(images[tile.tile_type], (tile_size, tile_size))
+			scaled_image = pygame.transform.scale(sprites[tile.tile_type], (tile_size, tile_size))
 			screen.blit(scaled_image, (col*tile_size, row*tile_size))
 
 	for location in active_piece.locations:
-		scaled_image = pygame.transform.scale(images[1], (tile_size, tile_size))
+		scaled_image = pygame.transform.scale(sprites[1], (tile_size, tile_size))
 		screen.blit(scaled_image, (location[0]*tile_size, location[1]*tile_size))
 
 
@@ -75,9 +94,6 @@ def update_board():
 		time_to_spawn = True
 		return
 
-	# for location in active_piece.locations:
-	# 	board[location[1]][location[0]] = Tile(tile_type = active_piece.piece_type, is_active = True)
-
 	allow_movement = True
 	if allow_movement:
 		if keys[pygame.K_LEFT]:
@@ -90,15 +106,15 @@ def update_board():
 			if can_move(direction = DIRECTION_DOWN):
 				active_piece.move(direction = DIRECTION_DOWN)
 
-
-
 	global debug_string
 	debug_string = ''
 	for location in active_piece.locations:
 		debug_string += str(location) + ' '
 	score += 1
 
-def can_move(direction = DIRECTION_DOWN):
+# Check if piece can move in the specified direction
+# Returns: True, False
+def can_move(direction):
 	return True # remove when fixed
 	# todo: insert logic to return True if none of the locations in piece would intersect a piece on the board after it were moved
 	if direction == DIRECTION_DOWN: # down
@@ -110,15 +126,14 @@ def can_move(direction = DIRECTION_DOWN):
 
 	return False
 
+# Check if lines can be cleared, clear them, shift stuff down, update score
 def clear_lines():
-	# todo: insert logic to check if line can be cleared and clear/move tiles
 	pass # do nothing
-
 
 
 pygame.init()
 
-load_images()
+load_sprites()
 init_board()
 
 logo = pygame.image.load('iconsmall.bmp')
