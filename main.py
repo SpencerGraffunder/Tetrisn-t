8714253e4f54b38e7c0bd7b0adf7538daaafdffc
Spencer Graffunder <spencer@SpencersMBP935.home>
2019-12-25 13:51:59 -0600

    move stuff all fixed probably

--- main.py
+++ main.py
@@ -96,7 +96,13 @@ class Game(States):
 		self.das_counter = 0
 		self.das_threshold = 0
 		self.down_counter = 0
+		self.is_move_right_pressed = False
+		self.is_move_left_pressed = False
+		self.is_move_down_pressed = False
+		self.spawn_delay_counter = 0
+		self.spawn_delay_threshold = 10
 		self.tetris_state = TETRIS_STATE_SPAWN
+
 		
 		self.sprites = {}
 
@@ -137,39 +143,68 @@ class Game(States):
 					if self.active_piece.can_rotate(self.board, ROTATION_CW):
 						self.active_piece.rotate(ROTATION_CW)
 						self.time_to_rotate = False
+						
+				if event.key == pygame.K_a:
+					self.is_move_left_pressed = True
+					self.das_threshold = 0
+					self.das_counter = 0
+				if event.key == pygame.K_d:
+					self.is_move_right_pressed = True
+					self.das_threshold = 0
+					self.das_counter = 0
+				if event.key == pygame.K_s:
+					self.is_move_down_pressed = True
+					self.down_counter = 0
+				self.time_to_move = False
+				
+		if event.type == pygame.KEYUP:
+			if event.key == pygame.K_a:
+				self.is_move_left_pressed = False
+			if event.key == pygame.K_d:
+				self.is_move_right_pressed = False
+			if event.key == pygame.K_s:
+				self.is_move_down_pressed = False
 			
 
 	def update(self, screen, dt):
 
+		keys = pygame.key.get_pressed()
+		self.is_move_left_pressed = keys[pygame.K_a]
+		self.is_move_right_pressed = keys[pygame.K_d]
+
+
 		if self.tetris_state == TETRIS_STATE_SPAWN:
-			# Spawn piece
-			# RNG piece choice decision
-			if self.next_piece_type == None:
-				self.active_piece_type = random.choice([PIECE_TYPE_I,PIECE_TYPE_O,PIECE_TYPE_T,PIECE_TYPE_L,PIECE_TYPE_J,PIECE_TYPE_Z,PIECE_TYPE_S])
-			else:
-				self.active_piece_type = self.next_piece.piece_type
-			self.next_piece_type = random.choice([PIECE_TYPE_I,PIECE_TYPE_O,PIECE_TYPE_T,PIECE_TYPE_L,PIECE_TYPE_J,PIECE_TYPE_Z,PIECE_TYPE_S])
-			if self.next_piece_type == self.active_piece_type:
+
+			self.spawn_delay_counter += 1
+
+			if self.spawn_delay_counter > self.spawn_delay_threshold:
+
+				# Spawn piece
+				# RNG piece choice decision
+				if self.next_piece_type == None:
+					self.active_piece_type = random.choice([PIECE_TYPE_I,PIECE_TYPE_O,PIECE_TYPE_T,PIECE_TYPE_L,PIECE_TYPE_J,PIECE_TYPE_Z,PIECE_TYPE_S])
+				else:
+					self.active_piece_type = self.next_piece.piece_type
 				self.next_piece_type = random.choice([PIECE_TYPE_I,PIECE_TYPE_O,PIECE_TYPE_T,PIECE_TYPE_L,PIECE_TYPE_J,PIECE_TYPE_Z,PIECE_TYPE_S])
-			self.active_piece = Piece(self.active_piece_type)
-			self.next_piece   = Piece(self.next_piece_type)
-			self.tetris_state = TETRIS_STATE_PLAY
-			self.fall_counter = 0
+				if self.next_piece_type == self.active_piece_type:
+					self.next_piece_type = random.choice([PIECE_TYPE_I,PIECE_TYPE_O,PIECE_TYPE_T,PIECE_TYPE_L,PIECE_TYPE_J,PIECE_TYPE_Z,PIECE_TYPE_S])
+				self.active_piece = Piece(self.active_piece_type)
+				self.next_piece   = Piece(self.next_piece_type)
+				self.tetris_state = TETRIS_STATE_PLAY
+				self.fall_counter = 0
+				self.spawn_delay_counter = 0
 
 		elif self.tetris_state == TETRIS_STATE_PLAY:
-
-			keys = pygame.key.get_pressed()
-
 			# Move piece logic
-			if keys[pygame.K_a] or keys[pygame.K_d]:
+			if self.is_move_left_pressed or self.is_move_right_pressed:
 				self.das_counter += 1
 			
 				if self.das_counter > self.das_threshold:
-					if keys[pygame.K_a]:
+					if self.is_move_left_pressed:
 						if self.active_piece.can_move(self.board, DIRECTION_LEFT):
 							self.active_piece.move(DIRECTION_LEFT)
 							self.das_counter = 0
-					if keys[pygame.K_d]:
+					if self.is_move_right_pressed:
 						if self.active_piece.can_move(self.board, DIRECTION_RIGHT):
 							self.active_piece.move(DIRECTION_RIGHT)
 							self.das_counter = 0
@@ -179,19 +214,20 @@ class Game(States):
 					else:
 						self.das_threshold = 6
 						
-			if keys[pygame.K_s]:
+			if self.is_move_down_pressed:
 				self.down_counter += 1
 				
 				if self.down_counter > 2:
-					if self.active_piece.can_move(self.board, DIRECTION_DOWN):
-						self.active_piece.move(DIRECTION_DOWN)
-						self.fall_counter = 0
-					else: # Lock piece
-						for location in self.active_piece.locations:
-							self.board[location[1]][location[0]] = Tile(self.active_piece.tile_type)
-						self.active_piece = None
-						self.tetris_state = TETRIS_STATE_CLEAR
-					self.down_counter = 0
+					if self.is_move_down_pressed:
+						if self.active_piece.can_move(self.board, DIRECTION_DOWN):
+							self.active_piece.move(DIRECTION_DOWN)
+							self.fall_counter = 0
+						else: # Lock piece
+							for location in self.active_piece.locations:
+								self.board[location[1]][location[0]] = Tile(self.active_piece.tile_type)
+							self.active_piece = None
+							self.tetris_state = TETRIS_STATE_CLEAR
+						self.down_counter = 0
 
 			self.fall_counter += 1
 
@@ -210,6 +246,8 @@ class Game(States):
 		elif self.tetris_state == TETRIS_STATE_CLEAR:
 			# Store all lines that can be cleared
 			lines_to_clear = []
+
+			self.is_move_down_pressed = False
 
 			# Add all clearable lines to list
 			for row_index, row in enumerate(self.board):
