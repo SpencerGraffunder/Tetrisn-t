6d98509acb2d1db50dd8ac4a1748efde7590d8b4
Brian <brianjbillman@gmail.com>
2019-12-27 19:09:13 -0600

    random pointless fixes

--- Piece.py
+++ Piece.py
@@ -38,13 +38,13 @@ class Piece:
 			self.locations[0] = (center-1,2) # [-][-][-] | [-][0][-] | [-][3][-] | [-][2][-]
 			self.locations[1] = (center,2)   # [0][1][2] | [3][1][-] | [2][1][0] | [-][1][3]
 			self.locations[2] = (center+1,2) # [-][3][-] | [-][2][-] | [-][-][-] | [-][0][-]
-			self.locations[3] = (center,3)   #			  |			  |			  |
+			self.locations[3] = (center,3)   #			 |			 |		     |
 			self.tile_type = TILE_TYPE_IOT
 		elif self.piece_type == PIECE_TYPE_L:
 			self.locations[0] = (center-1,2) # [-][-][-] | [3][0][-] | [-][-][3] | [-][2][-]
 			self.locations[1] = (center,2)   # [0][1][2] | [-][1][-] | [2][1][0] | [-][1][-]
 			self.locations[2] = (center+1,2) # [3][-][-] | [-][2][-] | [-][-][-] | [-][0][3]
-			self.locations[3] = (center-1,3) # 		  |		      |		      |
+			self.locations[3] = (center-1,3) # 		     |	         |	         |
 			self.tile_type = TILE_TYPE_LZ
 		elif self.piece_type == PIECE_TYPE_J:
 			self.locations[0] = (center-1,2) # [-][-][-] | [-][0][-] | [3][-][-] | [-][2][3]
@@ -62,7 +62,7 @@ class Piece:
 			self.locations[0] = (center,2)   # [-][-][-] | [-][1][-]
 			self.locations[1] = (center+1,2) # [-][0][1] | [-][0][3]
 			self.locations[2] = (center-1,3) # [2][3][-] | [-][-][2]
-			self.locations[3] = (center,3)   # 		     |
+			self.locations[3] = (center,3)   #           |
 			self.tile_type = TILE_TYPE_JS
 
 
@@ -114,21 +114,6 @@ class Piece:
 	
 		if self.piece_type == PIECE_TYPE_O: # for the meme
 			pass
-		# elif self.piece_type == PIECE_TYPE_Z: # the special case
-		# 	if rotation in [0,180]:
-		# 		pivot = copy(locations[1])
-		# 		locations[0] = (pivot[0]+1,pivot[1]-1)
-		# 		locations[1] = (pivot[0]+1,pivot[1])
-		# 		locations[2] = pivot
-		# 		locations[3] = (pivot[0],pivot[1]+1)
-		# 		rotation = (rotation+90) % 360
-		# 	elif rotation in [90,270]:
-		# 		pivot = copy(locations[2])
-		# 		locations[0] = (pivot[0]-1,pivot[1])
-		# 		locations[1] = pivot
-		# 		locations[2] = (pivot[0],pivot[1]+1)
-		# 		locations[3] = (pivot[0]+1,pivot[1]+1)
-		# 		rotation = (rotation+90) % 360
 		elif self.piece_type == PIECE_TYPE_I or self.piece_type == PIECE_TYPE_S or self.piece_type == PIECE_TYPE_Z: # the two-rotation-position pieces
 			if self.piece_type == PIECE_TYPE_I:
 				pivot = locations[2]
