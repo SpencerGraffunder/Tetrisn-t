3e026584d995054d2a03d44eff598c9224589252
Spencer Graffunder <spencergraffunder@cedarville.edu>
2020-02-26 18:38:17 -0600

    add trace setting key

--- Game.py
+++ Game.py
@@ -65,6 +65,10 @@ class Game(States):
 
 
 	def do_event(self, event):
+    
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_BACKQUOTE:
+                pdb.set_trace()
 	
 		for player_number in [0, 1]:
 			if event.type == pygame.KEYDOWN:
