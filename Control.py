df1a43757bfb619c56000c25b7590a51017d713c
Spencer Graffunder <asymmetriccube@gmail.com>
2020-05-16 20:28:43 -0500

    remove previous state thing

--- Control.py
+++ Control.py
@@ -8,26 +8,30 @@ class Control:
 		self.done = False
 		self.screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 		self.clock = pygame.time.Clock()
+
 	def setup_states(self, state_dict, start_state):
 		self.state_dict = state_dict
 		self.state_name = start_state
 		self.state = self.state_dict[self.state_name]
+
 	def update(self, dt):
 		if self.state.quit:
 			self.done = True
 		elif self.state.done:
 			self.flip_state()
 		self.state.update(self.screen, dt)
+
 	def flip_state(self):
 		self.state.done = False
-		previous, self.state_name = self.state_name, self.state.next
+		self.state_name = self.state.next
 		self.state = self.state_dict[self.state_name]
-		self.state.previous = previous
+
 	def event_loop(self):
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				self.done = True
 			self.state.do_event(event)
+
 	def main_game_loop(self):
 		while not self.done:
 			delta_time = self.clock.tick(FRAME_RATE)/1000.0
--- States.py
+++ States.py
@@ -1,12 +1,8 @@
 class States(object):
 
-	player_count = 0
-	board_width = 14
-	current_level = 0
-
 	def __init__(self):
 		self.just_started = True
 		self.done = False
 		self.next = None
 		self.quit = False
-		self.previous = None
+		self.state_change_data = {}
