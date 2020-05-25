62c8504771ac0ccea2ad372f6c2e70f59900159d
Spencer Graffunder <spencergraffunder@cedarville.edu>
2020-05-24 19:55:00 -0500

    make level selection work with arrow keys to any level

--- Level_Selection_Menu.py
+++ Level_Selection_Menu.py
@@ -9,8 +9,8 @@ class Level_Selection_Menu(States):
 
     def __init__(self):
         States.__init__(self)
-        self.next = 'game'
-        
+        self.level_selection = 0
+
 
     def do_event(self, event):
         if event.type == pygame.KEYDOWN:
@@ -18,23 +18,14 @@ class Level_Selection_Menu(States):
                 # ESC pressed
                 self.switch('main menu')
                 return
-
-            try:
-                # Get the value of the key pressed and cast to int
-                Globals.CURRENT_LEVEL = int(pygame.key.name(event.key))
-            except ValueError:
-                # Value was not an int so just return and wait for a real int
-                return
-
-            # add 10 if LSHIFT is pressed
-            keys = pygame.key.get_pressed()
-            if keys[pygame.K_LSHIFT] or keys[pygame.K_RSHIFT]:
-                Globals.CURRENT_LEVEL += 10
-
-            # start the game
-            Globals.GAME_JUST_STARTED = True
-
-            self.done = True
+            elif event.key == pygame.K_UP:
+                self.level_selection += 1
+            elif event.key == pygame.K_DOWN:
+                self.level_selection -= 1
+            elif event.key == pygame.K_SPACE or event.key == pygame.K_RETURN or event.key == pygame.K_KP_ENTER:
+                Globals.GAME_JUST_STARTED = True
+                Globals.CURRENT_LEVEL = self.level_selection
+                self.switch('game')
 
 
     def update(self, dt):
@@ -43,13 +34,15 @@ class Level_Selection_Menu(States):
 
     def draw(self, screen):
         screen.fill((150, 150, 150))
-        
+
         title_string = 'TETRISN\'T'
         subtitle_string = 'not a tetris game'
-        level_string_line1 = 'Enter the starting level\'s number'
-        level_string_line2 = 'Hold LSHIFT to add 10'
+        instruction_line1 = 'press UP or DOWN to change starting level'
+        instruction_line2 = 'press SPACE to start'
+        selection_string = str(self.level_selection)
 
-        text.draw(screen, title_string,    'LARGE', (Globals.WINDOW_WIDTH//2, Globals.WINDOW_HEIGHT//4),   (0, 160, 0))
-        text.draw(screen, subtitle_string, 'SMALL', (Globals.WINDOW_WIDTH//2, Globals.WINDOW_HEIGHT*2//4), (0, 120, 0))
-        text.draw(screen, level_string_line1, 'SMALL', (Globals.WINDOW_WIDTH*1//2, Globals.WINDOW_HEIGHT*3//4 - 15), (150, 0, 0))
-        text.draw(screen, level_string_line2, 'SMALL', (Globals.WINDOW_WIDTH*1//2, Globals.WINDOW_HEIGHT*3//4 + 15), (150, 0, 0))
+        text.draw(screen, title_string,      'LARGE', (Globals.WINDOW_WIDTH//2, Globals.WINDOW_HEIGHT   //4),  (0, 160, 0))
+        text.draw(screen, subtitle_string,   'SMALL', (Globals.WINDOW_WIDTH//2, Globals.WINDOW_HEIGHT*2 //4),  (0, 120, 0))
+        text.draw(screen, instruction_line1, 'SMALL', (Globals.WINDOW_WIDTH//2, Globals.WINDOW_HEIGHT*5 //8),  (150, 0, 0))
+        text.draw(screen, instruction_line2, 'SMALL', (Globals.WINDOW_WIDTH//2, Globals.WINDOW_HEIGHT*11//16), (150, 0, 0))
+        text.draw(screen, selection_string,  'LARGE', (Globals.WINDOW_WIDTH//2, Globals.WINDOW_HEIGHT*13//16), (150, 0, 0))
