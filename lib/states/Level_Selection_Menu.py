03313ae6164b9182c0a4a22951fe2cb75be93cd6
Spencer Graffunder <spencergraffunder@cedarville.edu>
2020-05-24 19:55:00 -0500

    make level selection work with arrow keys to any level

--- lib/states/Level_Selection_Menu.py
+++ lib/states/Level_Selection_Menu.py
@@ -10,49 +10,51 @@ class Level_Selection_Menu(States):
 
     def __init__(self):
         States.__init__(self)
-        self.next = 'client game'
+        self.level_selection = 0
 
     def update(self, dt):
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     # ESC pressed
-                    self.next = 'main menu'
+                    self.switch('main menu')
+                    return
+
+                if event.key == pygame.K_ESCAPE:
+                    # ESC pressed
+                    self.switch('main menu')
+                    return
+                elif event.key == pygame.K_UP:
+                    self.level_selection += 1
+                elif event.key == pygame.K_DOWN:
+                    self.level_selection -= 1
+                elif event.key == pygame.K_SPACE or event.key == pygame.K_RETURN or event.key == pygame.K_KP_ENTER:
+                    Globals.GAME_JUST_STARTED = True
+                    Globals.CURRENT_LEVEL = self.level_selection
+
+                    # start the game
+                    Globals.GAME_JUST_STARTED = True
+                    player_input = PlayerInput()
+                    player_input.start_game()
+                    player_input.set_starting_level(Globals.CURRENT_LEVEL)
+                    player_input.set_player_count(Globals.PLAYER_COUNT)
+                    Globals.connection.add_input(player_input)
+                    self.switch('client game')
+
                     self.done = True
-                    return
-
-                try:
-                    # Get the value of the key pressed and cast to int
-                    Globals.CURRENT_LEVEL = int(pygame.key.name(event.key))
-                except ValueError:
-                    # Value was not an int so just return and wait for a real int
-                    return
-
-                # add 10 if LSHIFT is pressed
-                keys = pygame.key.get_pressed()
-                if keys[pygame.K_LSHIFT] or keys[pygame.K_RSHIFT]:
-                    Globals.CURRENT_LEVEL += 10
-
-                # start the game
-                Globals.GAME_JUST_STARTED = True
-                player_input = PlayerInput()
-                player_input.start_game()
-                player_input.set_starting_level(Globals.CURRENT_LEVEL)
-                player_input.set_player_count(Globals.PLAYER_COUNT)
-                Globals.connection.add_input(player_input)
-
-                self.done = True
 
     def draw(self, screen):
         screen.fill((150, 150, 150))
-        
+
         title_string = 'TETRISN\'T'
         subtitle_string = 'not a tetris game'
-        level_string_line1 = 'Enter the starting level\'s number'
-        level_string_line2 = 'Hold LSHIFT to add 10'
+        instruction_line1 = 'press UP or DOWN to change starting level'
+        instruction_line2 = 'press SPACE to start'
+        selection_string = str(self.level_selection)
 
-        text.draw(screen, title_string,    'LARGE', (Globals.WINDOW_WIDTH//2, Globals.WINDOW_HEIGHT//4),   (0, 160, 0))
-        text.draw(screen, subtitle_string, 'SMALL', (Globals.WINDOW_WIDTH//2, Globals.WINDOW_HEIGHT*2//4), (0, 120, 0))
-        text.draw(screen, level_string_line1, 'SMALL', (Globals.WINDOW_WIDTH*1//2, Globals.WINDOW_HEIGHT*3//4 - 15), (150, 0, 0))
-        text.draw(screen, level_string_line2, 'SMALL', (Globals.WINDOW_WIDTH*1//2, Globals.WINDOW_HEIGHT*3//4 + 15), (150, 0, 0))
+        text.draw(screen, title_string,      'LARGE', (Globals.WINDOW_WIDTH//2, Globals.WINDOW_HEIGHT   //4),  (0, 160, 0))
+        text.draw(screen, subtitle_string,   'SMALL', (Globals.WINDOW_WIDTH//2, Globals.WINDOW_HEIGHT*2 //4),  (0, 120, 0))
+        text.draw(screen, instruction_line1, 'SMALL', (Globals.WINDOW_WIDTH//2, Globals.WINDOW_HEIGHT*5 //8),  (150, 0, 0))
+        text.draw(screen, instruction_line2, 'SMALL', (Globals.WINDOW_WIDTH//2, Globals.WINDOW_HEIGHT*11//16), (150, 0, 0))
+        text.draw(screen, selection_string,  'LARGE', (Globals.WINDOW_WIDTH//2, Globals.WINDOW_HEIGHT*13//16), (150, 0, 0))
         pygame.display.update()
