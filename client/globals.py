8829c640b8158dc03e70c78c246c7fafa8142eca
Spencer Graffunder <spencergraffunder@cedarville.edu>
2020-06-06 12:34:33 -0500

    make keybindings for 3 players

--- client/globals.py
+++ client/globals.py
@@ -23,17 +23,17 @@ keybindings[0] = {pygame.K_a: ControlType.LEFT,
                   pygame.K_q: ControlType.CCW,
                   pygame.K_e: ControlType.CW,
                   pygame.K_ESCAPE: ControlType.PAUSE}
-keybindings[1] = {pygame.K_j: ControlType.LEFT,
+keybindings[1] = {pygame.K_f: ControlType.LEFT,
+                  pygame.K_h: ControlType.RIGHT,
+                  pygame.K_g: ControlType.DOWN,
+                  pygame.K_r: ControlType.CCW,
+                  pygame.K_y: ControlType.CW,
+                  pygame.K_ESCAPE: ControlType.PAUSE}
+keybindings[2] = {pygame.K_j: ControlType.LEFT,
                   pygame.K_l: ControlType.RIGHT,
                   pygame.K_k: ControlType.DOWN,
                   pygame.K_u: ControlType.CCW,
                   pygame.K_o: ControlType.CW,
-                  pygame.K_ESCAPE: ControlType.PAUSE}
-keybindings[2] = {pygame.K_z: ControlType.LEFT,
-                  pygame.K_x: ControlType.RIGHT,
-                  pygame.K_c: ControlType.DOWN,
-                  pygame.K_v: ControlType.CCW,
-                  pygame.K_b: ControlType.CW,
                   pygame.K_ESCAPE: ControlType.PAUSE}
 
 # The graphics surfaces that will be drawn for each of the different tile types
