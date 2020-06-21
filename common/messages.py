cb349a045636261e6e7407eee0e9a1eaa6b0d5f5
bSkwared <laskyblake@gmail.com>
2020-06-21 08:18:02 -0700

    Add new client message

--- common/messages.py
+++ common/messages.py
@@ -2,13 +2,19 @@ from enum import Enum
 
 class Message:
     class Type(Enum):
-        PLAYER_NUMBERS = 1
-        GAME_STATE = 2
-        PLAYER_INPUT = 3
+        NEW_CLIENT = 1
+        PLAYER_NUMBERS = 2
+        GAME_STATE = 3
+        PLAYER_INPUT = 4
 
     def __init__(self, type_):
         self.type = type_
 
+
+class NewClientMessage(Message):
+    def __init__(self, local_player_count):
+        self.type = self.Type.NEW_CLIENT
+        self.data = {"new_client": local_player_count}
 
 class PlayerNumbersMessage(Message):
     def __init__(self, player_numbers):
--- server/connection.py
+++ server/connection.py
@@ -8,6 +8,7 @@ import common.messages as m
 class Connection:
     def __init__(self):
         self.player_inputs = []
+        self.new_client = 0
         self.state = None
         self.lock = Lock()
 
@@ -17,11 +18,18 @@ class Connection:
     def add_player_numbers(self, player_numbers):
         m.client_inbox.append(m.PlayerNumbersMessage(player_numbers))
 
+    def get_new_client_local_players(self):
+        self.__read_messages()
+        return self.new_client_local_players
+
     def get_player_input(self):
+        self.__read_messages()
         return self.player_inputs.pop(0) if self.player_inputs else PlayerInput(None)
 
     def __read_messages(self):
         while m.server_inbox:
             msg = m.server_inbox.pop(0)
-            if msg.type == m.Message.Type.PLAYER_INPUT:
+            if msg.type == m.Message.Type.NEW_CLIENT:
+                self.new_client_local_players = msg.data['new_client']
+            elif msg.type == m.Message.Type.PLAYER_INPUT:
                 self.player_inputs.append(msg.data['player_inputs'])
