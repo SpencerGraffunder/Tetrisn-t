bf6c722ad522ee5a1c27a5ee31d7f2b4ef9342d6
brian-j-billman <brianjbillman@gmail.com>
2020-06-26 21:21:32 -0400

    better spawn column; not perfect

--- server/states/game.py
+++ server/states/game.py
@@ -85,15 +85,16 @@ class Game(State):
         self.state.players = [Player(x) for x in range(self.state.player_count)]
 
         # split the board into PLAYER_COUNT equal sections (using floats), find the middle of the section we care about
-        # using the average, and round down for the left half, and [round up for the right half for an even number of
-        # players, round down for the right half for an odd number of players]
+        # using the average, and round down for the left half, and round up for the right half
         # that way the pieces hopefully don't overlap spawn positions and things stay symmetrical
-        # somehow it seems to work this way between 1-6 players (except 5?), so hopefully that will port well into 7+
+        # somehow it seems to work this way between 1-5 players (6 has some odd stuff; I'd bet it's some number theory
+        # throwing it off, and perhaps it'll be really difficult and different stuff must be implemented),
+        # so hopefully that will port well into 7+ after it's fixed in a general way for 6 player
         # this is all based on I piece spawn position, to help make all the 4 columns 'allocated' to each player be as
         # evenly spread as possible
         if self.state.player_count > 1:
             for player in self.state.players:
-                player.spawn_column = int(((self.state.board_width / self.state.player_count) * player.player_number + (self.state.board_width / self.state.player_count) * (player.player_number + 1)) / 2) + ((self.state.player_count + 1) % 2) * player.player_number // ((self.state.player_count + 1) // 2)
+                player.spawn_column = round((((self.state.board_width - 1) / self.state.player_count) * player.player_number + (self.state.board_width / self.state.player_count) * (player.player_number + 1)) / 2) #+ ((self.state.player_count + 1) % 2) * player.player_number // ((self.state.player_count + 1) // 2)
         elif self.state.player_count == 1:
             self.state.players[0].spawn_column = self.state.board_width // 2
 
